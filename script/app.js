2f8e9033447fce8c96dd7e2a356098df4d0a342f
rohithramesh44 <rohithramesh44@gmail.com>
2022-11-23 21:51:57 +0530

    menu page animation bug fixed

--- script/app.js
+++ script/app.js
@@ -1,10 +1,28 @@
-const menuBtn = document.querySelecor(".menu-icon");
-const menuBtn = document.querySelector("");
-let tl = gsap.timeline();
-const handleMenu = () => {
-  tl.to(".menu-page", { y: 0, ease: "elastic" });
+const menuBtn = document.querySelector(".menu-icon");
+const aaaBtn = document.querySelector(".aaa");
+let menuOpenTl = gsap.timeline({ reversed: true });
+
+menuBtn.addEventListener("click", () => {
+  animateOpenNav();
+});
+
+const animateOpenNav = () => {
+  menuOpenTl
+    .to(".menu-page", {
+      ease: "power4.out(1, 0.3)",
+      x: 0,
+      display: "flex",
+      duration: 0.3,
+    })
+    .to(".nav-item", {
+      x: 0,
+      opacity: 1,
+      duration: 0.1,
+      stagger: { each: 0.1, ease: "power2.easeInOut" },
+    });
+  menuOpenTl.reversed(!menuOpenTl.reversed());
 };
-menuBtn.addEventListener("click", handleeMenu);
+
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   loop: true,
